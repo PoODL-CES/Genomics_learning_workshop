2e426f2b2d9b08c3a3c0d2164ad83a547310735b
anubhabkhan <anubhabkhan@gmail.com>
2024-11-05 10:47:06 +0530

    Update Linux_basics.sh

--- Linux_basics.sh
+++ Linux_basics.sh
@@ -1,8 +1,8 @@
 ##### Task 1
-##### Create the following table in a file named test_table1.txt on terminal
+#### Create the following table in a file named test_table1.txt on terminal
 1  2  3  4  5
 6  7  8  9  10 
 11  12  13  14
 15  16  17  18
 
-##### Solution
+### Solution
