dde9e715b36568d50ce7fd80a418b8b21c8510f4
Pranav-Datar <pranavdatar01@gmail.com>
2024-11-05 10:57:47 +0530

    Update Linux_basics.sh

--- Linux_basics.sh
+++ Linux_basics.sh
@@ -5,4 +5,11 @@
 11  12  13  14
 15  16  17  18
 
-### Solution
+### Solution (pranav)
+touch test_table.txt #creates a new text file in the directory
+{
+    printf "%2d  %2d  %2d  %2d  %2d\n" 1 2 3 4 5
+    printf "%2d  %2d  %2d  %2d  %2d\n" 6 7 8 9 10
+    printf "%2d  %2d  %2d  %2d\n" 11 12 13 14
+    printf "%2d  %2d  %2d  %2d\n" 15 16 17 18
+} > test_table.txt # ">" forwards the entered table data into test_table.txt
