7bea84723d11d8d7e36d572a21041b8cade8dc0b
Haradhan2000 <rharadhan@iisc.ac.in>
2024-12-02 13:03:11 +0530

    Update fastq_operations.sh

--- fastq_operations.sh
+++ fastq_operations.sh
@@ -1,5 +1,9 @@
 #### Download the datasets from 
 
 ### Count the number of sequencing reads in the fastq files
+solution: for LGS1_sub_1.fq
+grep '^@' LGS1_sub_1.fq | wc -l ### grep '^@' Finds all lines starting with @ in the file.
+^@: Matches lines where @ appears at the beginning.
+wc -l: Counts the number of matching lines.
 
 
