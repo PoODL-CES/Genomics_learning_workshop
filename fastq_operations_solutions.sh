0ca0a461688872a16985e133c6fd9a6402e019f6
anubhabkhan <anubhabkhan@gmail.com>
2024-12-02 14:49:04 +0530

    Update fastq_operations_solutions.sh

--- fastq_operations_solutions.sh
+++ fastq_operations_solutions.sh
@@ -13,14 +13,14 @@ AAFFFJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFJJJJJJJJJJJJFJJJJJJJJJJFJJJJJJJJJJJJJJJJJJ
 ### Solution
 ## 1
 grep '^@' LGS1_sub_1.fq | wc -l ### grep '^@' Finds all lines starting with @ in the file.
-^@: Matches lines where @ appears at the beginning.
-wc -l: Counts the number of matching lines.
+# ^@: Matches lines where @ appears at the beginning.
+# wc -l: Counts the number of matching lines.
 
 ## 2
 echo $(( $(wc -l < LGS1_sub_1.fq) / 4 ))
-echo: displays result of the arithmatic operation
-wc -1: counts the number of lines in LGS1_sub_1.fq
-dividing by 4 since one sequencing read has 4 lines of fastq file
+# echo: displays result of the arithmatic operation
+# wc -1: counts the number of lines in LGS1_sub_1.fq
+# dividing by 4 since one sequencing read has 4 lines of fastq file
 
 ## 3
 wc -l LGS1_sub_1.fq | awk '{print $1 / 4}'
