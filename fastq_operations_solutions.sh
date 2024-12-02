f8a8c09ee23cae72c715c169fb39335c236593b5
anubhabkhan <anubhabkhan@gmail.com>
2024-12-02 14:51:41 +0530

    Update fastq_operations_solutions.sh

--- fastq_operations_solutions.sh
+++ fastq_operations_solutions.sh
@@ -4,12 +4,11 @@
 # fastq files are the sequencing output files files obtained after demultimplexing data from illumina seqeuncers. Most sequencing companies provide this file format for illumina data.
 # It consists of four lines
 
-/*
+
 @SRR15369215.126490887 #sequence identifier 
 GGACCTTCTGTCATTTCACTCCTTCTGAAGTAAGGAGTGAAGTAAACACGAAGTAAACACGACAGGTTAGTCCTATTCCTTCAAGCAGGAGTACAGAAAAGAATGCAAATTCTGGGTTCTAGCCCAGCTTTTACTCCTATGGTTCTATTT #sequence
 + #seperator
 AAFFFJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFJJJJJJJJJJJJFJJJJJJJJJJFJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFAFJJJJJJJJJJJJJJJJFJJJJJJJJJFJJJJJJJJJFFFJJJJJJJJJJJ #base call quality scores (ASCII characters)
-*/
 
 ### Count the number of sequencing reads in the fastq files
 
