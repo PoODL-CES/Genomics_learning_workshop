a16b0b886e2d12d9365118c84329a0b10660cb31
Pranav-Datar <pranavdatar01@gmail.com>
2024-12-02 13:09:50 +0530

    Update fastq_operations.sh

--- fastq_operations.sh
+++ fastq_operations.sh
@@ -6,4 +6,8 @@ grep '^@' LGS1_sub_1.fq | wc -l ### grep '^@' Finds all lines starting with @ in
 ^@: Matches lines where @ appears at the beginning.
 wc -l: Counts the number of matching lines.
 
-
+solution2: for LGS1_sub_1.fq
+echo $(( $(wc -l < LGS1_sub_1.fq) / 4 ))
+echo: displays result of the arithmatic operation
+wc -1: counts the number of lines in LGS1_sub_1.fq
+dividing by 4 since one sequencing read has 4 lines of fastq file
