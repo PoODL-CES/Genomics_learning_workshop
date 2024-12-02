46fc336582bf23c99691d787fa7db494904fd18a
anubhabkhan <anubhabkhan@gmail.com>
2024-12-02 14:49:25 +0530

    Update fastq_operations_solutions.sh

--- fastq_operations_solutions.sh
+++ fastq_operations_solutions.sh
@@ -12,7 +12,8 @@ AAFFFJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFJJJJJJJJJJJJFJJJJJJJJJJFJJJJJJJJJJJJJJJJJJ
 
 ### Solution
 ## 1
-grep '^@' LGS1_sub_1.fq | wc -l ### grep '^@' Finds all lines starting with @ in the file.
+grep '^@' LGS1_sub_1.fq | wc -l 
+# grep '^@' Finds all lines starting with @ in the file.
 # ^@: Matches lines where @ appears at the beginning.
 # wc -l: Counts the number of matching lines.
 
