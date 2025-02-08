d02d2c0c8296138156bc03f30e83a01c24b27d2b
Pranav-Datar <pranavdatar01@gmail.com>
2025-02-08 15:12:14 +0530

    Update Read_mapping_and_sorting.sh

--- Read_mapping_and_sorting.sh
+++ Read_mapping_and_sorting.sh
@@ -19,6 +19,8 @@ done
 
 
 ## Deactivate the conda environment
+conda deactivate
+
 
 ## EXPLAIN THE SAMTOOLS OPTIONS USED. WHY IS THIS STEP NECESSARY?
 samtools view -S -b BEN_NW_10_aligned_reads.sam > BEN_NW_10_aligned_reads.bam
