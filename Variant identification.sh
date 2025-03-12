5b12df7d4858fafbc6d77b03a977f2b4d7b87e78
ternithinator <136674078+ternithinator@users.noreply.github.com>
2025-03-12 13:05:33 +0530

    Update Variant identification.sh

--- Variant identification.sh
+++ Variant identification.sh
@@ -5,6 +5,14 @@ conda create -n strelka -c bioconda strelka
 conda activate strelka
 #Activates strelka
 
+#Index the reference genome file (skip it if you already have .fai index file)
+samtools faidx GCA_021130815.1_PanTigT.MC.v3_genomic.fna
+#faidx stands for FASTA indexing.
+
+#Strelka is run in a 2 step procedure
+
+# Step:1 - Configuration - to specify the input data and any options pertaining to the variant calling methods themselves
+
 which configureStrelkaGermlineWorkflow.py
 #If Strelka2 is installed properly, this command should return the full path
 
@@ -14,11 +22,28 @@ which configureStrelkaGermlineWorkflow.py
 #  --referenceFasta reference_genome_filename.fna Provides the reference genome in FASTA format. This is the genome against which the BAM file was aligned.
 #--runDir strelka_germline: Specifies the output directory where the workflow will be set up. Inside this directory, Strelka will create scripts and configuration files needed to run the variant calling pipeline.
 
-#Index the reference genome file (skip it if you already have .fai index file)
-samtools faidx GCA_021130815.1_PanTigT.MC.v3_genomic.fna
-#faidx stands for FASTA indexing.
+# Step:2 - Workflow Execution - to specify parameters pertaining to how strelka is executed
 
 strelka_germline/runWorkflow.py -m local -j 8
 #strelka_germline/runWorkflow.py: This script was generated during the configureStrelkaGermlineWorkflow.py step. It is the main script that runs the variant calling process.
 #-m local: Specifies that the workflow will be executed on the local machine.
 #-j 8: Defines the number of parallel threads (CPUs) to use. 
+
+# For loop command for multiple files: takes in bam files iteratively and performs configuration and execution steps inside the loop
+
+for bam in *_aligned_reads_sorted_deduplicated.bam; do  # loops through the required bam files
+    sample_name=$(basename "$bam" _aligned_reads_sorted_deduplicated.bam) # extracts the sample name from the .bam filename
+    output_dir="strelka_germline_${sample_name}" 
+    
+    mkdir -p "$output_dir"  # make a directory with the sample name extracted in the above steps
+
+    /home/nithinka/miniconda3/envs/strelka/bin/configureStrelkaGermlineWorkflow.py \
+        --bam "$bam" \
+        --referenceFasta GCA_021130815.1_PanTigT.MC.v3_genomic.fna \
+        --runDir "$output_dir"                                                      # run configuration step
+
+    "$output_dir/runWorkflow.py" -m local -j 8                                      # workflow execution step
+
+done
+
+# an output directory will be present for all input bam files containing results of variant calling.
