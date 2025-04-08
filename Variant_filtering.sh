0a8247fa1c22f307228c64270ff369a9b0bb780c
Pranav-Datar <pranavdatar01@gmail.com>
2025-04-08 15:10:58 +0530

    Update Variant_filtering.sh

--- Variant_filtering.sh
+++ Variant_filtering.sh
@@ -92,3 +92,6 @@ R -e "library(ggplot2); library(scales); data <- read.table('variant_counts.txt'
 
 vcftools --vcf machali_Aligned_rangeWideMerge_strelka_update2_BENGAL_mac3_passOnly_biallelicOnly_noIndels_minMAF0Pt05_chr_E2_minDP3_minQ30_minGQ30_hwe_0.05.recode.vcf --max-missing 0.6 --out machali_Aligned_rangeWideMerge_strelka_update2_BENGAL_mac3_passOnly_biallelicOnly_noIndels_minMAF0Pt05_chr_E2_minDP3_minQ30_minGQ30_hwe_0.05_mm0.6 --recode
 
+#find the percentile depth between 0.05 and 0.975, the middle 95 percentile.
+#below 0.05 because low conf
+#above 97.5 because of presence of repetitive regions or less complex regions
