efcd9e56504d494258ffea528aa09651ba8768a2
Pranav-Datar <pranavdatar01@gmail.com>
2025-04-11 15:27:04 +0530

    Update Linux_advanced.sh

--- Linux_advanced.sh
+++ Linux_advanced.sh
@@ -1,7 +1,14 @@
 #### Download the datasets from https://zenodo.org/records/14258052
+#fastq reads are in .gz format
+#genome files are in .fna file (fna is fasta)
+#fna.amb: stores information about the presence of ambiguous bases (e.g., N, or other non-standard nucleotides) in the reference genome.
+#fna.ann:  stores information about the reference genome, including sequence names, lengths, and other metadata.
+#fna.bwt: contains the Burrows-Wheeler transformed sequence, a compressed representation of the reference genome.
+#fna.pac: contains packed sequence data
+#fna.sa: suffix array index, used to locate positions of sequences within the reference genome.
 
 ### Structure of fastq files
-# fastq files are the sequencing output files files obtained after demultimplexing data from illumina seqeuncers. Most sequencing companies provide this file format for illumina data.
+# fastq files are the sequencing output files files obtained after demultimplexing data from illumina seqeuncers. Most sequencing companies provide this file format for illumina data in ".gz" extension.It is primarily used for reducing the size of a single file, often on UNIX-like systems.
 # It consists of four lines
 
 
