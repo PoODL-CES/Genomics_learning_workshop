841d1ae8d0e5799b152a90974f48d2d37d461e60
Pranav-Datar <pranavdatar01@gmail.com>
2025-04-19 12:22:00 +0530

    Update Read_mapping_and_sorting_solutions.sh

--- Read_mapping_and_sorting_solutions.sh
+++ Read_mapping_and_sorting_solutions.sh
@@ -1,4 +1,4 @@
-#### Map trimmed reads using bwa mem
+####Task 1: Map trimmed reads using bwa mem
 ## The datasets from https://zenodo.org/records/14258052 contain the reference genome file and its index files
 
 ### Install bwa
